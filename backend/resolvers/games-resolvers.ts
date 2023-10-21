f229c31f8a4dc69e94aa683598675a6bf9111883
paultlehmann <swellwrench@gmail.com>
2023-10-21 00:51:26 -0500

    All wheres working in checkForGames

--- backend/resolvers/games-resolvers.ts
+++ backend/resolvers/games-resolvers.ts
@@ -4,24 +4,46 @@ import { pool } from '..';
 
 // const { app, pool } = serverInfo
 
+type TGameStage = 'pre' | 'offer' | 'accept' | 'post';
+
+interface IGameQueryOptions {
+  admin?: number;
+  participants?: number[];
+  stage?: TGameStage;
+}
+
+interface IAdjustedValues {
+  admin?: number;
+  participants?: string;
+  stage?: `'${TGameStage}'`;
+}
+
 export const checkForGamesResolver = () => (req: Request, res: Response) => {
   // console.log('resolver hit');
-  const { admin, participants, stage } = req.body;
+  const { admin, participants, stage }: IGameQueryOptions = req.body;
 
-  const wheres = Object.keys(req.body).map(
-    (key: string, index: number) =>
-      `${index === 0 ? 'where' : ' and'} ${key} = ${req.body[key]}`
-  );
+  const adjustedValues: IAdjustedValues = {
+    admin,
+    participants: `'{${participants?.join(',')}}'`,
+    stage: stage && `'${stage}'`
+  };
+
+  const wheres = Object.keys(req.body)
+    .map(
+      (key: string, index: number) =>
+        // @ts-ignore
+        `${index === 0 ? 'where' : ' and'} ${key} = ${adjustedValues[key]}`
+    )
+    ?.join('');
 
   console.log('wheres', wheres);
+  console.log('query', `SELECT * FROM games ${wheres}`);
 
   // const checkForGames = async () => {
-  pool
-    .query("SELECT * FROM games WHERE stage = 'pre'")
-    .then((result: QueryResult) => {
-      // console.log('checkForGames result', result);
-      return res.status(200).send(result.rows);
-    });
+  pool.query(`SELECT * FROM games ${wheres}`).then((result: QueryResult) => {
+    // console.log('checkForGames result', result);
+    return res.status(200).send(result.rows);
+  });
   // };
 
   // return checkForGames();
--- frontend/src/components/GameLobby.tsx
+++ frontend/src/components/GameLobby.tsx
@@ -3,11 +3,13 @@ import { checkForGames } from '../queries/games';
 
 const GameLobby = (props: any) => {
   const admin = 6;
+  const participants = [1, 2, 3, 4, 5];
+  const stage = 'pre';
   return (
     <>
       <h1>No games found!</h1>
       <ButtonWithRefresh
-        onClick={() => checkForGames({ admin })}
+        onClick={() => checkForGames({ admin, participants, stage })}
         text={'Check Again'}
       />
     </>
