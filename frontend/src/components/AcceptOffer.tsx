556dcad35a1bf60bdce221e5de65c5aca0f7b06f
paultlehmann <swellwrench@gmail.com>
2023-10-25 08:53:05 -0500

    AcceptOffer screen created

--- frontend/src/components/AcceptOffer.tsx
+++ frontend/src/components/AcceptOffer.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { Button, Card, CardContent, CardHeader, Grid } from '@mui/material';
+import _ from 'lodash';
 import { getOffers } from '../queries/offers';
 import { IGameState, IOffer } from '../types';
 
@@ -7,16 +9,69 @@ interface IProps {
   userId: number;
 }
 
+interface IOpponentHistory {
+  accepted: boolean;
+  amount: number;
+  round: number;
+}
+
 const AcceptOffer = (props: IProps) => {
   const { gameState, userId } = props;
 
   const [offer, setOffer] = useState<IOffer | null>(null);
+  const [opponentHistory, setOpponentHistory] = useState<IOpponentHistory[]>(
+    []
+  );
 
   useEffect(
     () => getOffers(setOffer, gameState.id, gameState.round, undefined, userId),
     []
   );
-  return <div>{`Offer: ${offer?.amount}`}</div>;
+  // return <div>{`Offer: ${offer?.amount}`}</div>;
+  return (
+    offer && (
+      <Card>
+        <CardHeader title={'Accept or Reject Offer'} />
+        <CardContent>
+          <div>You have been offered:</div>
+          <div style={{ fontSize: '32px' }}>
+            $<span style={{ color: 'green' }}>{offer.amount}</span>
+          </div>
+          <div>
+            (other player gets $
+            <span style={{ color: 'red' }}>{10 - offer.amount}</span>)
+          </div>
+          <div>by a player with the following history:</div>
+          <div>
+            {!_.isEmpty(opponentHistory)
+              ? opponentHistory.map(
+                  (historyItem: IOpponentHistory) =>
+                    `Round ${historyItem.round}: ${
+                      historyItem.accepted ? 'Accepted' : 'Rejected'
+                    } an offer of $${historyItem.amount}`
+                )
+              : 'None Yet'}
+          </div>
+          <Grid
+            container={true}
+            direction={'row'}
+            style={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              marginTop: '5px'
+            }}
+          >
+            <Grid item={true}>
+              <Button variant={'contained'}>Accept</Button>
+            </Grid>
+            <Grid item={true}>
+              <Button variant={'contained'}>Reject</Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    )
+  );
 };
 
 export default AcceptOffer;
