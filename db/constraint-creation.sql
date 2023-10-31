2bcd4b231b15bd9ead6ca7bdd25f2f6c0563f72a
paultlehmann <swellwrench@gmail.com>
2023-10-31 12:54:25 -0500

    Unique constraint

--- db/constraint-creation.sql
+++ db/constraint-creation.sql
@@ -3,3 +3,6 @@ ADD CONSTRAINT fk_recipient_users FOREIGN KEY (recipient_id) REFERENCES users (i
 
 ALTER TABLE offers
 ADD CONSTRAINT fk_offerer_users FOREIGN KEY (offerer_id) REFERENCES users (id);
+
+ALTER TABLE offers
+ADD CONSTRAINT unique_offer UNIQUE (game_id, round_number, offerer_id);
